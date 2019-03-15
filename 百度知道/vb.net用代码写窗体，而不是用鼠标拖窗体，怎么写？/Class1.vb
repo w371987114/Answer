ecbe9c39d57facfd82963b013d0d0281e0238c4a
Shiney <007@wangshuai.app>
2019-03-15 11:59:52 +0800

    添加答案代码

---
+++ 百度知道/vb.net用代码写窗体，而不是用鼠标拖窗体，怎么写？/Class1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.ComponentModel
+
+Partial Class Class1
+    Inherits System.Windows.Forms.Form
+    '构造函数
+    Public Sub New()
+        '在构造函数中调用InitializeComponent方法以初始化控件
+        InitializeComponent()
+    End Sub
+
+
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(361, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "class1"
+        Me.Text = "class1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+
+
+End Class
---
+++ 百度知道/vb.net用代码写窗体，而不是用鼠标拖窗体，怎么写？/Program.vb
@@ -0,0 +1,12 @@
+﻿'此为主函数， 需在项目的启动对象中选择此module（要先取消勾选应用程序框架）
+Module mainModule
+
+    Sub Main()
+
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Class1)
+
+    End Sub
+End Module
+
